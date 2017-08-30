10b237b62a5ec396c17f90cd9ef8fe02d5016eb4
internetseekho <internetseekho.com@gmail.com>
2017-08-30 17:23:21 +0500

    Init
    The code you can use for block breaker game

---
+++ Assets/scripts/LevelManager.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelManager : MonoBehaviour {
+
+	public void LoadLevel( string name ) {
+
+        print( "Load Level " + name );
+        Application.LoadLevel( name );
+
+    }
+
+    public void QuitRequest() {
+        print("Quit request received");
+        Application.Quit();
+    }
+}
